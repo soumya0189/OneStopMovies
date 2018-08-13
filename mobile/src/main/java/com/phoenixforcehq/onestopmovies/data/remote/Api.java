98ef6fbb8802ad9dcc44ec86a8979d2251ff64e0
soumya0189 <soumya010689@gmail.com>
2018-08-14 04:08:40 +0530

    API integration : APIs for listing popular, tp rated, now playing and upcoming movies.

--- mobile/src/main/java/com/phoenixforcehq/onestopmovies/data/remote/Api.java
+++ mobile/src/main/java/com/phoenixforcehq/onestopmovies/data/remote/Api.java
@@ -15,5 +15,23 @@ public interface Api {
             @Query("language") String lang,
             @Query("page") int page);
 
+    @GET("/3/movie/top_rated")
+    Observable<MovieListResponse> getTopRatedMovies(
+            @Query("api_key") String publicKey,
+            @Query("language") String lang,
+            @Query("page") int page);
+
+    @GET("/3/movie/now_playing")
+    Observable<MovieListResponse> getNowPlayingMovies(
+            @Query("api_key") String publicKey,
+            @Query("language") String lang,
+            @Query("page") int page);
+
+    @GET("/3/movie/upcoming")
+    Observable<MovieListResponse> getUpcomingMovies(
+            @Query("api_key") String publicKey,
+            @Query("language") String lang,
+            @Query("page") int page);
+
 
 }
